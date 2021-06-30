adfb04305ae232b6d02aa9bfb7657c41f2bcf98a
sandeep2194 <sandeep2194@live.com>
2021-06-30 10:17:45 +0530

    results done

---
+++ src/components/resultItem.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Container, Row, Col, ProgressBar } from 'react-bootstrap'
+
+const Result = (props) => {
+    const { option1, option2, results } = props.poll
+    const option1Score = results.option1
+    const option2Score = results.option2
+    const totalScore = option1Score + option2Score
+    return (
+        <Container className='bg-light'>
+            <Col className='py-4 px-2'>
+                <h5>{'Would you rather ' + option1 + ' ?'}</h5>
+                <ProgressBar now={option1Score / totalScore * 100} />
+                <p>{option1Score + ' out of ' + totalScore}</p>
+            </Col>
+            <Col className='py-4 px-2'>
+                <h5>{'Would you rather ' + option2 + ' ?'}</h5>
+                <ProgressBar now={option2Score / totalScore * 100} />
+                <p>{option2Score + ' out of ' + totalScore}</p>
+            </Col>
+        </Container>
+    )
+}
+
+export default Result
--- src/components/results.js
+++ src/components/results.js
@@ -1,22 +1,30 @@
 import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Image } from 'react-bootstrap'
 import { connect } from 'react-redux'
-
+import { PersonCircle } from 'react-bootstrap-icons'
+import Result from './resultItem'
 const Results = (props) => {
+    const { name, pic } = props.user
     return (
         <Container className='my-5'>
             <Row className="justify-content-center">
                 <Col className='border rounded py-3' lg={5}>
                     <Row className="justify-content-start border-bottom pb-2 px-3">
-                        <h5>asked By Tyler</h5>
+                        <h5>{name + ' asks'}</h5>
                     </Row>
                     <Col className='py-3 pb-4'>
-                        <Row>
-                            <Col>
-
+                        <Row className="justify-content-center align-items-center">
+                            <Col sm={3} className='m-2 px-3 border-right'>
+                                <Row className="justify-content-center">
+                                    {pic ?
+                                        <Image src={pic} height={72} width={72} className='rounded-circle' alt='profile pic' />
+                                        :
+                                        <PersonCircle size={72} />
+                                    }
+                                </Row>
                             </Col>
                             <Col>
-
+                                <Result poll={props.poll} />
                             </Col>
                         </Row>
                     </Col>
@@ -30,10 +38,13 @@ const Results = (props) => {
     )
 }
 
-function mapStateToProps({ polls }, props) {
+function mapStateToProps({ polls, users }, props) {
     const { pollId } = props.match.params
+    const poll = polls[pollId]
+    const user = users[poll.authorId]
     return {
-        poll: polls[pollId]
+        poll,
+        user,
     }
 }
 
